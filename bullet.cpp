880c3f378223bc8e5d2d6b8fbfab5e6e06c666aa
namath <ntran91@gmail.com>
2013-04-27 00:12:37 -0300

    Update bullet.cpp

--- bullet.cpp
+++ bullet.cpp
@@ -1,176 +1,213 @@
+//------Global Members-------
 const int playerbullet = 0;
 
 SDL_Rect clipPlayerbullet[1];
 
+
+//----------CLASS Declaration-------------
 class bullet
 {
-  public:
+	public:
 
-	//----------------------
-	//----Constructors
-	//----------------------
+		//----------------------
+		//----Constructors
+		//----------------------
     
-	bullet();
-	bullet(int x, int y, int w, int h);
+		bullet();
+		bullet(int x, int y, int w, int h);
 	
-	//----------------------
-	//----Facilitators
-	//----------------------
+		//----------------------
+		//----Mutators
+		//----------------------
 
-	//void setyVel_bullet(int yVel_bull);
-	void setclips();
-	void setstatus(bool stat);
-	void handle_input();
-	void move();
-	void setXY(int x, int y);
-	void setWH(int w, int h);
-	void setcounter(int j);
+		void setXY(int x, int y);
+		void setWH(int w, int h);
+		void setYVel(int yVel);
+		void setXVel(int xVel);
+		void setClips();
+		void setOnScreen(bool state);
 
-	//----------------------
-	//----Inspectors
-	//----------------------
+		//----------------------
+		//----Inspectors
+		//----------------------
 	
-	void getbullets(SDL_Surface *surface);
-	bool getstatus() const;
-	int gety_bullet() const;
-	int getcounter() const;
+		int getX() const;
+		int getY() const;
+		int getW() const;
+		int getH() const;
+		int getXVel() const;
+		int getYVel() const;
+		void getBullet(SDL_Surface *surface);
+		bool getOnScreen() const;
 
-	void show();
+		//----------------------
+		//----Facilitators
+		//----------------------
+
+		void move();
+		void show();
+		void handle_input(int x, int y);
+
+
+	//----------------------
+	//----Private Members
+	//----------------------
 
 	private:
 
-	int xVel_bullet, yVel_bullet;
-	int counter;
+		// Movements
+		int x_, y_, w_, h_;
+		int xVel_, yVel_;
 
-	bool status;
-	SDL_Rect proj;
-	SDL_Surface *bullets;
+		// Graphics
+		bool onScreen;
+		SDL_Rect proj;
+		SDL_Surface *bulletSprite;
 };
 
-bullet::bullet()
-{
-	xVel_bullet = 0;
-	yVel_bullet = 0;
-	counter = 0;
-	bullets = character;
-}
 
-bullet::bullet(int x, int y, int w, int h)
-{
-	proj.x = x;
-	proj.y = y;
-	proj.w = w;
-	proj.h = h;
-	xVel_bullet = 0;
-	yVel_bullet = 0;
-	counter = 0;
-	bullets = character;
-}
 
+
+//----------------------
+//----Constructors
+//----------------------
+
+	bullet::bullet()
+	{
+		xVel_ = 0;
+		yVel_ = 0;
+		onScreen = false;
+		bulletSprite = character;
+	}
+
+	bullet::bullet(int x, int y, int w, int h)
+	{
+		proj.x = x;
+		proj.y = y;
+		proj.w = w;
+		proj.h = h;
+		xVel_ = 0;
+		yVel_ = 0;
+		onScreen = false;
+		bulletSprite = character;
+	}
+
+
+//----------------------
+//----Mutators
+//----------------------
+
+	void bullet::setXY(int x, int y)
+	{
+		proj.x = x;
+		proj.y = y;
+	}
+
+	void bullet::setWH(int w, int h)
+	{
+		proj.w = w;
+		proj.h = h;
+	}
+
+
+	void bullet::setClips()
+	{
+		clipPlayerbullet [0].x = 128;
+		clipPlayerbullet [0].y = 0;
+		clipPlayerbullet [0].w = 15;
+		clipPlayerbullet [0].h = 15;
+	}
+
+	void bullet::setXVel(int xVel)
+	{
+		xVel_ = xVel;
+	}
+
+	void bullet::setYVel(int yVel)
+	{
+		yVel_ = yVel;
+	}
+
+	void bullet::setOnScreen(bool state)
+	{
+		onScreen = state;
+	}
 //----------------------
 //----Inspectors
 //----------------------
 
-bool bullet::getstatus() const
-{
-	return status;
-}
-
-int bullet::gety_bullet() const
-{
-	return proj.y;
-}
-
-int bullet::getcounter() const
-{
-	return counter;
-}
-
-void bullet::show()
-{
-	if (bullet::getstatus() == true)
+	int bullet::getX() const
 	{
-		apply_surface(proj.x, proj.y, bullets, screen, &clipPlayerbullet[0]);
+		return x_;
 	}
-}
+	int bullet::getY() const
+	{
+		return y_;
+	}
+	int bullet::getW() const
+	{
+		return w_;
+	}
+	int bullet::getH() const
+	{
+		return h_;
+	}
+
+	int bullet::getXVel() const
+	{
+		return xVel_;
+	}
+
+	int bullet::getYVel() const
+	{
+		return yVel_;
+	}
+
+	bool bullet::getOnScreen() const
+	{
+		return onScreen;
+	}
 
 //----------------------
 //----Facilitator
 //----------------------
 
-//void bullet::setyVel_bullet(int yVel_bull)
-//{
-//	yVel_bullet = yVel_bull;
-//}
+	void bullet::move()
+	{
+		//Move the dot up or down
+		proj.y -= yVel_;
 
-void bullet::setstatus(bool stat)
-{
-	status = stat;
-	//status[0] = stat[0];
-	//status[1] = stat[1];
-	//status[2] = stat[2];
-	//status[3] = stat[3];
-	//status[4] = stat[4];
-	//status[5] = stat[5];
-	//status[6] = stat[6];
-	//status[7] = stat[7];
-	//status[8] = stat[8];
-	//status[9] = stat[9];
-}
+		//If the bullet went too far up or down
+		//if ((proj.y < -15) || (proj.y + proj.h > SCREEN_HEIGHT + 15) )
+		//{
+		//	//remove bullet
+		//	setOnScreen(false);
+		//};
+	}
 
-void bullet::setXY(int x, int y)
-{
-	proj.x = x;
-	proj.y = y;
-}
+	void bullet::show()
+	{
+		if (getOnScreen() == true)
+		{
+			//cout << "This is " << getOnScreen() << endl;
+			apply_surface(proj.x, proj.y, bulletSprite, screen, &clipPlayerbullet[0]);
+		}
+	}
+	
+	void bullet::handle_input(int x, int y)
+	{
+		Uint8 *keystates = SDL_GetKeyState (NULL);
 
-void bullet::setWH(int w, int h)
-{
-	proj.w = w;
-	proj.h = h;
-}
-
-void bullet::setcounter(int j)
-{
-	counter = j;
-}
-
-void bullet::setclips()
-{
-	clipPlayerbullet [0].x = 128;
-	clipPlayerbullet [0].y = 0;
-	clipPlayerbullet [0].w = 15;
-	clipPlayerbullet [0].h = 15;
-}
-
-void bullet::handle_input()
-{
-			status = true;
-			yVel_bullet = 15;
-	//Uint8 *keystates = SDL_GetKeyState (NULL);
-
- //   //If a key was pressed
- //   if (event.type == SDL_KEYDOWN)
- //   {
- //       //Adjust the velocity
-	//	if (event.key.keysym.sym == SDLK_z)
- //       {
-	//		status = true;
-	//		yVel_bullet = 15;
-	//	}
-	//}
-}
-
-void bullet::move()
-{
-    //Move the bullet up
-    proj.y -= yVel_bullet;
-
-    //If the bullet went too far up or down
-    if ((proj.y < -15) || (proj.y + proj.h > SCREEN_HEIGHT + 15) )
-    {
-        //remove bullet
-        status = false;
-    }
-}
-
+		//If a key was pressed
+		if (event.type == SDL_KEYDOWN)
+		{
+			//Adjust the velocity
+			if (event.key.keysym.sym == SDLK_z)
+			{
+				cout << "z was pressed" << endl;
+				setOnScreen(true);
+				setXY(x, y);
+				yVel_= 15;
+			}
+		}
+	}
